a2915a92796aaed290e7fa74dc7428c2b2fef3b4
elton_cit <eltonm@ciandt.com>
2016-01-13 08:31:48 -0200

    Correção dos imports css e javascript nas paginas e tradução em todas paginas

--- src/main/webapp/resources/js/translations.js
+++ src/main/webapp/resources/js/translations.js
@@ -1,5 +1,5 @@
 angular.module('gettext').run(['gettextCatalog', function (gettextCatalog) {
 /* jshint -W100 */
-    gettextCatalog.setStrings('pt_BR', {"Accept the license agreement":"Aceitar o contrato de licença","Dashboard":"Painel","Forgot password?":"Esqueceu a senha?","Keep me signed in!":"Mantenha-me logado!","Login":"Entrar","Please, enter the email to which the new password will be sent":"Por favor, insira o e-mail para o qual a nova senha será enviada","Register":"Registrar"});
+    gettextCatalog.setStrings('pt_BR', {"Accept the license agreement":"Aceitar o contrato de licença","Confirm password":"Confirme a senha","Dashboard":"Painel","Email":"Email","Email Address":"Endereço de email","Forgot password?":"Esqueceu a senha?","Keep me signed in!":"Mantenha-me logado!","Login":"Entrar","Nah.. it's 404":"Nah.. é 404","New password":"Nova senha","New password again":"Nova senha denovo","Password":"Senha","Please, enter the email to which the new password will be sent":"Por favor, insira o e-mail para o qual a nova senha será enviada","Register":"Registrar"});
 /* jshint +W100 */
 }]);
